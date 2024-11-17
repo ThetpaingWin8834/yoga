afab296680874f7befc886ee8b3998e4e00ba12e
ThetpaingWin8834 <www.thetpaingwin.tpw@gmail.com>
2024-11-17 20:19:10 +0630

    Update booking_list_screen.dart

--- lib/screens/booking_list_screen.dart
+++ lib/screens/booking_list_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../models/class_data.dart';
+import '../utils.dart';
+
 class BookingListScreen extends StatefulWidget {
   const BookingListScreen({super.key});
 
@@ -9,6 +12,15 @@ class BookingListScreen extends StatefulWidget {
 }
 
 class _BookingListScreenState extends State<BookingListScreen> {
+  bool isLoading = true;
+  Object? error;
+  List<ClassItem> classlist = [];
+  @override
+  void initState() {
+    super.initState();
+    getAllClasses();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,13 +30,102 @@ class _BookingListScreenState extends State<BookingListScreen> {
       ),
       body: Column(
         children: [
-          Row(
-            children: [
-              const Expanded(child: CupertinoTextField()),
-              IconButton(onPressed: () {}, icon: const Icon(Icons.search))
-            ],
-          )
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 4),
+            child: Row(
+              children: [
+                const Expanded(
+                    child: CupertinoTextField(
+                  placeholder: 'Day of week of time',
+                )),
+                const SizedBox(width: 12),
+                FilledButton.tonal(
+                  onPressed: () {},
+                  child: const Text('Search'),
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            child: isLoading
+                ? const Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : error != null
+                    ? Center(
+                        child: Text(error.toString()),
+                      )
+                    : classlist.isEmpty
+                        ? const Center(
+                            child: Text('There is no available class'),
+                          )
+                        : ListView.builder(
+                            itemBuilder: (context, index) {
+                              return BookingClassItemWidget(
+                                  classData: classlist[index]);
+                            },
+                            itemCount: classlist.length,
+                          ),
+          ),
         ],
+      ),
+    );
+  }
+
+  void getAllClasses() async {
+    setState(() {
+      isLoading = true;
+      error = null;
+    });
+    await Future.delayed(const Duration(seconds: 2));
+    setState(() {
+      isLoading = false;
+      error = null;
+      classlist = List.generate(
+        100,
+        (index) => ClassItem(
+            id: index.toString(),
+            courseId: index.toString(),
+            dateOfClass: formatDateToString(DateTime.now()),
+            teacher: 'Teacher $index',
+            comments: 'comment'),
+      );
+    });
+  }
+
+  @override
+  void setState(VoidCallback fn) {
+    if (mounted) super.setState(fn);
+  }
+}
+
+class BookingClassItemWidget extends StatelessWidget {
+  final ClassItem classData;
+  const BookingClassItemWidget({
+    super.key,
+    required this.classData,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final themeData = Theme.of(context);
+    return Card.outlined(
+      margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'Teacher : ${classData.teacher}',
+              style: themeData.textTheme.labelLarge,
+            ),
+            Text('Date : ${classData.dateOfClass}'),
+            Text('Comment : ${classData.comments}'),
+            const SizedBox(height: 12),
+          ],
+        ),
       ),
     );
   }
