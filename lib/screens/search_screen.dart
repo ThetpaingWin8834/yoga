6d6739c038df7c0f8187613f16d85b9b39196bed
ThetpaingWin8834 <www.thetpaingwin.tpw@gmail.com>
2024-11-18 23:11:15 +0630

    Update search_screen.dart

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:yoga/models/search_item.dart';
 
 class SearchScreen extends StatefulWidget {
@@ -10,7 +13,7 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
-  bool isLoading = true;
+  bool isLoading = false;
   Object? error;
   List<SearchItem> searchList = [];
   final List<SearchItem> _allData = [];
@@ -18,7 +21,7 @@ class _SearchScreenState extends State<SearchScreen> {
   @override
   void initState() {
     super.initState();
-    getAllClasses();
+    // getAllClasses();
   }
 
   @override
@@ -31,28 +34,30 @@ class _SearchScreenState extends State<SearchScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: CupertinoTextField(
-          controller: _controller,
-          placeholder: 'Search...',
-          onChanged: onSearch,
-        ),
+        title: const Text('Search'),
         centerTitle: true,
       ),
       body: Column(
         children: [
-          // Padding(
-          //   padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 4),
-          //   child: Row(
-          //     children: [
-          //       Expanded(
-          //           child: CupertinoTextField(
-          //         controller: _controller,
-          //         placeholder: 'Day of week of time',
-          //         onChanged: onSearch,
-          //       )),
-          //     ],
-          //   ),
-          // ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 4),
+            child: Row(
+              children: [
+                Expanded(
+                    child: CupertinoTextField(
+                  controller: _controller,
+                  placeholder: 'Enter day of week',
+                )),
+                const SizedBox(width: 12),
+                FilledButton.tonal(
+                  onPressed: () {
+                    onSearch(_controller.text.trim());
+                  },
+                  child: const Text('Search'),
+                )
+              ],
+            ),
+          ),
           const SizedBox(height: 7),
           Expanded(
             child: isLoading
@@ -64,8 +69,10 @@ class _SearchScreenState extends State<SearchScreen> {
                         child: Text(error.toString()),
                       )
                     : searchList.isEmpty
-                        ? const Center(
-                            child: Text('There is no available class'),
+                        ? Center(
+                            child: Text(_controller.text.trim().isEmpty
+                                ? 'Search with day of week'
+                                : 'There is no available class'),
                           )
                         : ListView.builder(
                             itemBuilder: (context, index) {
@@ -80,25 +87,59 @@ class _SearchScreenState extends State<SearchScreen> {
     );
   }
 
-  void onSearch(String text) {
-    setState(
-      () {
-        if (text.isEmpty) {
-          searchList = _allData;
-        } else {
-          searchList = _allData.where((item) {
-            return item.dateOfClass
-                    .toLowerCase()
-                    .contains(text.toLowerCase()) ||
-                item.teacher.toLowerCase().contains(text.toLowerCase()) ||
-                item.locationOfClass
-                    .toLowerCase()
-                    .contains(text.toLowerCase()) ||
-                item.teacher.toLowerCase().contains(text.toLowerCase());
-          }).toList();
-        }
-      },
-    );
+  void onSearch(String text) async {
+    try {
+      if (text.isEmpty) {
+        ScaffoldMessenger.maybeOf(context)
+            ?.showSnackBar(const SnackBar(content: Text('Enter day of week')));
+        return;
+      }
+      setState(
+        () {
+          isLoading = true;
+          error = null;
+        },
+      );
+      final response = await http.post(
+          Uri.parse('http://10.0.2.2/flowfityoga/searchClass.php'),
+          body: jsonEncode({'day': text}));
+      final jsonList = jsonDecode(response.body) as List<dynamic>;
+      setState(
+        () {
+          isLoading = false;
+          error = null;
+          final temp = jsonList
+              .map<SearchItem>((json) => SearchItem.fromMap(json))
+              .toList();
+          searchList = temp;
+        },
+      );
+    } catch (e) {
+      setState(
+        () {
+          isLoading = false;
+          error = e.toString();
+        },
+      );
+    }
+    // setState(
+    //   () {
+    //     if (text.isEmpty) {
+    //       searchList = _allData;
+    //     } else {
+    //       searchList = _allData.where((item) {
+    //         return item.dateOfClass
+    //                 .toLowerCase()
+    //                 .contains(text.toLowerCase()) ||
+    //             item.teacher.toLowerCase().contains(text.toLowerCase()) ||
+    //             item.locationOfClass
+    //                 .toLowerCase()
+    //                 .contains(text.toLowerCase()) ||
+    //             item.teacher.toLowerCase().contains(text.toLowerCase());
+    //       }).toList();
+    //     }
+    //   },
+    // );
   }
 
   void getAllClasses() async {
